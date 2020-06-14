3580aa65995970cf96142eacef4e074fd6e57f87
Sidetrail <branchingtrails@gmail.com>
2020-06-13 23:39:41 -0500

    added more styling and completed nav bar

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -6,47 +6,53 @@ const ContactPage = props => {
   useSetTitle('Contact');
   return (
     <div className="contactPage">
-      <div>Contact Me</div>
-      {iconPair(
-        <i className="fab fa-instagram" />,
-        'Instagram',
-        'https://www.instagram.com/sidetrail/',
-      )}
-      {iconPair(<i className="fab fa-twitter" />, 'Twitter', 'https://twitter.com/_Sidetrail')}
-      {iconPair(
-        <i className="fab fa-flickr" />,
-        'Flikr',
-        'https://www.flickr.com/photos/188185390@N02/',
-      )}
-      {iconPair(
-        <i className="fab fa-reddit" />,
-        'Reddit',
-        'https://old.reddit.com/user/Sidetrail/',
-      )}
-      {iconPair(
-        <i className="fab fa-youtube" />,
-        'Youtube',
-        'https://www.youtube.com/channel/UCPon5JvcOh6hPLQFc6AUvuw',
-      )}
-      {iconPair(
-        <i className="fab fa-linkedin-in" />,
-        'LinkedIn',
-        'https://www.linkedin.com/in/john-t-williams/',
-      )}
-      {iconPair(
-        <i className="far fa-envelope" />,
-        'John_Williams@Sidetrail.io',
-        'mailto:John_Williams@Sidetrail.io',
-      )}
-      {iconPair(<i className="fab fa-github" />, 'Sidetrail', 'https://github.com/Sidetrail')}
+      <div className="title">Contact Me</div>
+      <div className="description">
+        While I may not be active on all of these accounts, I will see the notifications.
+      </div>
+      <div className="links">
+        <div>
+          <div className="subTitle">Social</div>
+          {iconPair(
+            <i className="fab fa-instagram" />,
+            'Instagram',
+            'https://www.instagram.com/sidetrail/',
+          )}
+          {iconPair(<i className="fab fa-twitter" />, 'Twitter', 'https://twitter.com/_Sidetrail')}
+          {iconPair(
+            <i className="fab fa-flickr" />,
+            'Flikr',
+            'https://www.flickr.com/photos/188185390@N02/',
+          )}
+          {iconPair(
+            <i className="fab fa-youtube" />,
+            'Youtube',
+            'https://www.youtube.com/channel/UCPon5JvcOh6hPLQFc6AUvuw',
+          )}
+        </div>
+        <div>
+          <div className="subTitle">Professional</div>
+          {iconPair(
+            <i className="fab fa-linkedin-in" />,
+            'LinkedIn',
+            'https://www.linkedin.com/in/john-t-williams/',
+          )}
+          {iconPair(
+            <i className="far fa-envelope" />,
+            'John_Williams@Sidetrail.io',
+            'mailto:John_Williams@Sidetrail.io',
+          )}
+          {iconPair(<i className="fab fa-github" />, 'Sidetrail', 'https://github.com/Sidetrail')}
+        </div>
+      </div>
     </div>
   );
 };
 
 const iconPair = (icon, name, url) => (
-  <a href={url}>
-    <div>{icon}</div>
-    <div>{name}</div>
+  <a href={url} className="socialLink">
+    <div className="linkIcon">{icon}</div>
+    <div className="linkName">{name}</div>
   </a>
 );
 
--- src/components/NavigationTabs/NavigationTabs.js
+++ src/components/NavigationTabs/NavigationTabs.js
@@ -3,21 +3,18 @@ import { Link, withRouter } from 'react-router-dom';
 import { navigationTabs } from '../../services/navigationTabs';
 import './NavigationTabs.scss';
 
-const NavigationTabs = props => {
-  console.log(props);
-  return (
-    <div className="navigationTabs">
-      {navigationTabs.map(tab => (
-        <Link
-          key={tab.url}
-          to={tab.url}
-          className={`tab ${props.match.path === tab.url ? 'tab-active' : 'tab-inacitve'}`}
-        >
-          {tab.name}
-        </Link>
-      ))}
-    </div>
-  );
-};
+const NavigationTabs = props => (
+  <div className="navigationTabs">
+    {navigationTabs.map(tab => (
+      <Link
+        key={tab.url}
+        to={tab.url}
+        className={`tab ${props.match.path === tab.url ? 'tab-active' : 'tab-inactive'}`}
+      >
+        {tab.name}
+      </Link>
+    ))}
+  </div>
+);
 
 export default withRouter(NavigationTabs);
