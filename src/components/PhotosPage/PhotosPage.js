b5f98b495974fbf64e686cb4299b8506f7c88476
Sidetrail <branchingtrails@gmail.com>
2020-06-14 14:17:02 -0500

    started with flickr client and photos page

--- src/components/PhotosPage/PhotosPage.js
+++ src/components/PhotosPage/PhotosPage.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getRecentPhotos } from '../../services/flickrClient';
 import './PhotosPage.scss';
 
-const PhotosPage = props => (
-	<div className="photosPage">
-		PhotosPage
-	</div>
-)
+const PhotosPage = props => {
+  useEffect(() => {
+    getRecentPhotos();
+  }, []);
+  return (
+    <div className="photosPage">
+      <div className="title">Photos</div>
+    </div>
+  );
+};
 
 export default PhotosPage;
---
+++ src/services/flickrClient.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_flickr_url;
+const BASE_URL = 'https://api.flickr.com/services/rest/?method=';
+const SIDETRAIL_USER_ID = '188185390@N02';
+
+const getUrlForMethod = method =>
+  `${BASE_URL + method}&api_key=${API_KEY}&per_page=10&format=json&nojsoncallback=1`;
+
+export const getRecentPhotos = () => {
+  axios
+    .get(getUrlForMethod(`flickr.people.getPhotos&user_id=${SIDETRAIL_USER_ID}`))
+    .then(resp => console.log(resp));
+};
+export const getGalleries = () => {};
